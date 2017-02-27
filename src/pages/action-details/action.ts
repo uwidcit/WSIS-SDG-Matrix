227531e628dce5bee03f0787e2c17c390a2610a4
kjcharles <kcreddevils@gmail.com>
2017-02-27 16:07:39 -0400

    Added action line icons and descriptions

--- src/pages/action-details/action.ts
+++ src/pages/action-details/action.ts
@@ -1,17 +1,16 @@
 import { SDG } from '../sdg-details/sdg';
 
 export class Action {
+    
     id: number;
     name: string;
     desc: string;
     rationales : string[];
     goals: SDG[];
-    iconUrl: string;
     
-    constructor(id: number, name: string, iconUrl: string) {
+    constructor(id: number, name: string) {
         this.id = id;
         this.name = name;
-        this.iconUrl = iconUrl;
         this.rationales = [];
         this.desc = "";
         this.goals = [];
