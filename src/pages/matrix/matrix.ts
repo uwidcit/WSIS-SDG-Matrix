74362b912e592b56c3c91d48530b5c76e473c871
kyledef <kyle.e.defreitas@gmail.com>
2018-11-09 21:58:18 -0400

    more testing required

--- src/pages/matrix/matrix.ts
+++ src/pages/matrix/matrix.ts
@@ -1,132 +1,140 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 
-import { AlertController, App, PopoverController } from 'ionic-angular';
-import { NavController } from 'ionic-angular';
+import {AlertController, App, NavController, PopoverController} from 'ionic-angular';
 
-import { Action } from '../action-details/action';
-import { ActionPopup } from '../action-popup/action-popup';
-import { SDG } from '../sdg-details/sdg';
-import { SDGPage } from '../sdg-details/sdg-details';
+import {Action} from '../action-details/action';
+import {ActionPopup} from '../action-popup/action-popup';
+import {SDG} from '../sdg-details/sdg';
+import {SDGPage} from '../sdg-details/sdg-details';
 
-import { SDGService } from '../../services/sdg.service';
-import { ActionService } from '../../services/action.service';
+import {SDGService} from '../../services/sdg.service';
+import {ActionService} from '../../services/action.service';
 
 @Component({
-    selector: 'page-matrix',
-    templateUrl: 'matrix.html',
-    providers: [SDGService, ActionService, Storage]
+  selector: 'page-matrix',
+  templateUrl: 'matrix.html',
+  providers: [SDGService, ActionService, Storage]
 })
 
 export class MatrixPage {
-    
-    goals: SDG[];
-    actions: Action[];
-    
-    ACTION_SHOWED: boolean;
-    GOAL_SHOWED: boolean;
-    
-    allGoals;
-    allActions;
-    errorMessage;
 
-    constructor(public navCtrl: NavController, public alertCtrl: AlertController, public sdgService: SDGService,
-                public actionService: ActionService, public app: App, public popoverCtrl: PopoverController) {
-        sdgService.getGoals()
-                    .subscribe(
-                         goals => this.goals = goals,
-                         error =>  this.errorMessage = <any>error
-                    );
-        
-        actionService.getActions()
-                    .subscribe(
-                         actions => this.actions = actions,
-                         error =>  this.errorMessage = <any>error
-                    );
+  @ViewChild('actions_sec') public action_el:ElementRef;
+  @ViewChild('goals_sec') public goal_el:ElementRef;
+
+  goals: SDG[];
+  actions: Action[];
+
+  ACTION_SHOWED: boolean;
+  GOAL_SHOWED: boolean;
+
+  allGoals;
+  allActions;
+  errorMessage;
+
+  constructor(public navCtrl: NavController,
+              public alertCtrl: AlertController,
+              public sdgService: SDGService,
+              public actionService: ActionService,
+              public app: App,
+              public popoverCtrl: PopoverController) {
+    sdgService.getGoals()
+      .subscribe(
+        goals => this.goals = goals,
+        error => this.errorMessage = <any>error
+      );
+
+    actionService.getActions()
+      .subscribe(
+        actions => this.actions = actions,
+        error => this.errorMessage = <any>error
+      );
+  }
+
+  goalTapped(event, goal) {
+    this.ACTION_SHOWED = false;
+    this.GOAL_SHOWED = true;
+
+    if (typeof this.allActions == "undefined") {
+      console.log("setting all hidden");
+      this.allActions = this.actions.slice();
+      this.setAllHidden(this.allActions, true);
     }
-    
-    goalTapped(event, goal){
-        this.ACTION_SHOWED = false;
-        this.GOAL_SHOWED = true;
-        
-        if(typeof this.allActions == "undefined"){
-            console.log("setting all hidden");
-            this.allActions = this.actions.slice();
-            this.setAllHidden(this.allActions, true);
-        }
-        
-        var idx = this.goals.indexOf(goal);
-        this.setAllHiddenExcept(this.goals, true, idx);
-        
-        this.setAllHidden(this.allActions, true);
-        this.actions = this.getElements(this.allActions, goal.actions);
-        
-        window.location.href = '#actions';
+
+    const idx = this.goals.indexOf(goal);
+    this.setAllHiddenExcept(this.goals, true, idx);
+
+    this.setAllHidden(this.allActions, true);
+    this.actions = this.getElements(this.allActions, goal.actions);
+
+    // window.location.href = '#actions';
+    this.action_el.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'start' });
+  }
+
+  actionTapped(event, action) {
+    this.ACTION_SHOWED = true;
+    this.GOAL_SHOWED = false;
+
+    if (typeof this.allGoals == "undefined") {
+      console.log("setting all hidden");
+      this.allGoals = this.goals.slice();
+      this.setAllHidden(this.allGoals, true);
     }
-    
-    actionTapped(event, action){
-        this.ACTION_SHOWED = true;
-        this.GOAL_SHOWED = false;
-        
-        if(typeof this.allGoals == "undefined"){
-            console.log("setting all hidden");
-            this.allGoals = this.goals.slice();
-            this.setAllHidden(this.allGoals, true);
-        }
-        
-        var idx = this.actions.indexOf(action);
-        this.setAllHiddenExcept(this.actions, true, idx);
-        
-        this.setAllHidden(this.allGoals, true);
-        this.goals = this.getGoals(this.allGoals, action.goals);
-        
-        window.location.href = '#goals';
+
+    const idx = this.actions.indexOf(action);
+    this.setAllHiddenExcept(this.actions, true, idx);
+
+    this.setAllHidden(this.allGoals, true);
+    this.goals = this.getGoals(this.allGoals, action.goals);
+
+    // window.location.href = '#goals';
+    this.goal_el.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'start' });
+  }
+
+  actionPressed(event, action) {
+    let popover = this.popoverCtrl.create(ActionPopup, {"goals": action.goals}, {cssClass: 'action-popover'});
+    popover.present();
+  }
+
+  goalPressed(event, goal) {
+    console.log("pressed");
+    this.app.getRootNav().push(SDGPage, {"goal": goal.id});
+  }
+
+  getElements(arr: any[], ids: any[]) {
+    for (let i = 0; i < ids.length; i++) {
+      arr[ids[i]].hidden = false;
     }
-    
-    actionPressed(event, action){
-        let popover = this.popoverCtrl.create(ActionPopup, {"goals": action.goals}, {cssClass: 'action-popover'});
-        popover.present();
+    return arr;
+  }
+
+  getGoals(arr: any[], ids: any[]) {
+    for (let i = 0; i < ids.length; i++) {
+      arr[ids[i].goalId].hidden = false;
     }
-    
-    goalPressed(event, goal){
-        console.log("pressed");
-        this.app.getRootNav().push(SDGPage, {"goal": goal.id});
+    return arr;
+  }
+
+  setAllHidden(arr: any[], state: boolean) {
+    for (let i = 0; i < arr.length; i++) {
+      arr[i].hidden = state;
     }
-    
-    getElements(arr: any[], ids: any[]){
-        for(var i = 0; i < ids.length; i++){
-            arr[ids[i]].hidden = false;
-        }
-        return arr;
+  }
+
+  setAllHiddenExcept(arr: any[], state: boolean, idx: number) {
+    for (let i = 0; i < arr.length; i++) {
+      if (i == idx)
+        arr[i].hidden = !state;
+      else
+        arr[i].hidden = state;
     }
-    
-    getGoals(arr: any[], ids: any[]){
-        for(var i = 0; i < ids.length; i++){
-            arr[ids[i].goalId].hidden = false;
-        }
-        return arr;
-    }
-    
-    setAllHidden(arr: any[], state: boolean){
-        for(var i = 0; i < arr.length; i++){
-            arr[i].hidden = state;
-        }
-    }
-    
-    setAllHiddenExcept(arr: any[], state: boolean, idx: number){
-        for(var i = 0; i < arr.length; i++){
-            if(i == idx)
-                arr[i].hidden = !state;
-            else
-                arr[i].hidden = state;
-        }
-    }
-    
-    reset(event){
-        console.log("resettings");
-        this.ACTION_SHOWED = false;
-        this.GOAL_SHOWED = false;
-        this.setAllHidden(this.goals, false);
-        this.setAllHidden(this.actions, false);
-    }
+  }
+
+  reset(event) {
+    console.log("resettings");
+    this.ACTION_SHOWED = false;
+    this.GOAL_SHOWED = false;
+    this.setAllHidden(this.goals, false);
+    this.setAllHidden(this.actions, false);
+  }
 
 }
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,105 +1,106 @@
-import { Component } from '@angular/core';
-import { NavController, AlertController, PopoverController, ViewController, App } from 'ionic-angular';
-import { TranslateService } from 'ng2-translate';
-import { InAppBrowser } from "@ionic-native/in-app-browser";
+import {Component} from '@angular/core';
+import {AlertController, App, NavController, PopoverController, ViewController} from 'ionic-angular';
+import {TranslateService} from 'ng2-translate';
+import {InAppBrowser} from "@ionic-native/in-app-browser";
 
-import { HomePage } from '../home/home';
-import { SDGListPage } from '../sdg-list/sdg-list';
-import { ActionListPage } from '../action-list/action-list';
-import { MatrixPage } from '../matrix/matrix';
-import { AboutPage } from '../about/about';
+import {HomePage} from '../home/home';
+import {SDGListPage} from '../sdg-list/sdg-list';
+import {ActionListPage} from '../action-list/action-list';
+import {MatrixPage} from '../matrix/matrix';
+import {AboutPage} from '../about/about';
 
 @Component({
-    selector: 'page-tabs',
-    templateUrl: 'tabs.html'
+  selector: 'page-tabs',
+  templateUrl: 'tabs.html'
 })
 
 export class TabPage {
-    
-    tab1: any;
-    tab2: any;
-    tab3: any;
-    tab4: any;
 
-    constructor(public alertCtrl: AlertController, public translate: TranslateService, 
-                public popoverCtrl: PopoverController, public app: App, private iab: InAppBrowser) {
-        this.tab1 = HomePage;
-        this.tab2 = ActionListPage;
-        this.tab3 = SDGListPage;
-        this.tab4 = MatrixPage;
-        
-        translate.setDefaultLang('en');
-    }
-    
-    openWebpage() {
-    
-        // Opening a URL and returning an InAppBrowserObject
-        const browser = this.iab.create('https://www.itu.int/net4/wsis/forum/2018/Pages/Agenda#agenda', '_system');
+  tab1: any;
+  tab2: any;
+  tab3: any;
+  tab4: any;
+
+  constructor(public alertCtrl: AlertController,
+              public translate: TranslateService,
+              public popoverCtrl: PopoverController,
+              public app: App,
+              private iab: InAppBrowser) {
+
+    this.tab1 = HomePage;
+    this.tab2 = ActionListPage;
+    this.tab3 = SDGListPage;
+    this.tab4 = MatrixPage;
+
+    translate.setDefaultLang('en');
+  }
+
+  openWebpage() {
+    // Opening a URL and returning an InAppBrowserObject
+    const browser = this.iab.create('https://www.itu.int/net4/wsis/forum/2018/Pages/Agenda#agenda', '_system');
+  }
+
+  pickLanguage() {
+    this.translate.get('LANGUAGES').subscribe(
+      value => {
+        // value is our translated string
+        console.log(value);
+        let alert = this.alertCtrl.create();
+        alert.setTitle('Language');
+        this.addLangs(value, alert);
+
+        alert.addButton('Cancel');
+        alert.addButton({
+          text: 'OK',
+          handler: data => {
+            console.log(data);
+            this.translate.use(data);
+          }
+        });
+        alert.present();
       }
+    );
 
-    pickLanguage() {
-        this.translate.get('LANGUAGES').subscribe(
-            value => {
-                // value is our translated string
-                console.log(value);
-                let alert = this.alertCtrl.create();
-                alert.setTitle('Language');
-                this.addLangs(value, alert)
-                
-                alert.addButton('Cancel');
-                alert.addButton({
-                    text: 'OK',
-                    handler: data => {
-                        console.log(data);
-                        this.translate.use(data);
-                    }
-                });
-                alert.present();
-            }
-        );
 
-       
+  }
+
+  addLangs(langs: any[], alert) {
+    var currLang = this.translate.getDefaultLang();
+    if (typeof this.translate.currentLang != "undefined")
+      currLang = this.translate.currentLang;
+
+    for (let i = 0; i < langs.length; i++) {
+      console.log(langs[i]);
+      alert.addInput({
+        type: 'radio',
+        label: langs[i].name,
+        value: langs[i].val,
+        checked: langs[i] == currLang
+      });
     }
-    
-    addLangs(langs :any[], alert){
-        var currLang = this.translate.getDefaultLang();
-        if(typeof this.translate.currentLang != "undefined")
-            currLang = this.translate.currentLang;
-        
-        for(let i = 0; i < langs.length; i++){
-            console.log(langs[i]);
-            alert.addInput({
-                type: 'radio',
-                label: langs[i].name,
-                value: langs[i].val,
-                checked: langs[i] == currLang
-            });
-        }
-    }
-    
-    showAbout() {
-        /*let popover = this.popoverCtrl.create(Menu);
-        popover.present();
-        */
-        
-        this.app.getRootNav().push(AboutPage, {});
-    }
+  }
+
+  showAbout() {
+    this.app.getRootNav().push(AboutPage, {});
+  }
 }
 
 @Component({
-    template: '<h3 style="padding-left: 16px; font-size: 1.1em;">Menu</h3><ion-list><span ion-item id="menu" *ngFor="let option of options" (click)="optionTapped(option)">{{ option }}</span></ion-list>'
+  template: '<h3 style="padding-left: 16px; font-size: 1.1em;">Menu</h3><ion-list><span ion-item id="menu" *ngFor="let option of options" (click)="optionTapped(option)">{{ option }}</span></ion-list>'
 })
 
 export class Menu {
-    
-    options = ["About"];
-    
-    constructor(public navCtrl: NavController, public viewCtrl: ViewController, public app: App) {
-        
-    }
-    
-    optionTapped() {
-        this.viewCtrl.dismiss();
-        this.app.getRootNav().push(AboutPage, {});
-    }
+
+  options = ["About"];
+
+  constructor(public navCtrl: NavController,
+              public viewCtrl: ViewController,
+              public app: App) {
+
+  }
+
+  optionTapped() {
+    this.viewCtrl.dismiss();
+    this.app.getRootNav().push(AboutPage, {});
+  }
 }
