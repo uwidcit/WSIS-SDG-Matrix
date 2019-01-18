40e927de370540f5caa5eecd67470c661c2e9f91
Kyle_ <kyle.e.defreitas@gmail.com>
2019-01-17 21:00:32 -0400

    Updated analytics to fix rendering issues

--- src/app/analytics/analytics.service.ts
+++ src/app/analytics/analytics.service.ts
@@ -14,22 +14,38 @@ export class AnalyticsService {
     this.is_debug = !environment.production;
   }
 
-  // noinspection JSMethodCanBeStatic
   startTrackerWithId(id) {
-    if (this.is_debug) { console.log('Attempting to start the GA analytics tracker'); }
-    if (ga && environment.use_tracking) { // It is possible that the google script is not loaded (e.g. ad blockers)
-      ga('create', {storage: 'none', trackingId: id, clientId: localStorage.getItem('ga:clientId')});
-      ga('set', 'checkProtocolTask', null);​
-      ga('set', 'connection', 'online');
-      ga('set', 'transportUrl', 'https://www.google-analytics.com/collect');
-      ga('set', 'platform_type', 'web');
-      ga(function (tracker) {
-        if (!localStorage.getItem('ga:clientId')) {
-          localStorage.setItem('ga:clientId', tracker.get('clientId'));
+    if (this.is_debug) {
+      console.log('Attempting to start the GA analytics tracker');
+    }
+    try {
+      if (typeof ga === 'function') {
+        if (environment.use_tracking) {
+          ga('create', {
+            storage: 'none',
+            trackingId: id,
+            clientId: localStorage.getItem('ga:clientId')
+          });
+          ga('set', 'checkProtocolTask', null);​
+          ga('set', 'connection', 'online');
+          ga('set', 'transportUrl', 'https://www.google-analytics.com/collect');
+          ga('set', 'platform_type', 'web');
+          ga((tracker) => {
+            if (!localStorage.getItem('ga:clientId')) {
+              localStorage.setItem('ga:clientId', tracker.get('clientId'));
+            }
+          });
+          if (this.is_debug) {
+            console.log('GA analytics tracker successfully initialised');
+          }
+          return true;
         }
-      });
-      if (this.is_debug) { console.log('GA analytics tracker successfully initialised'); }
-      return true;
+      }
+    } catch (e) {
+      if (this.is_debug) {
+        console.error("Unable to load analytics tracker: ");
+        console.error(e);
+      }
     }
     return false;
   }
@@ -40,11 +56,19 @@ export class AnalyticsService {
    * @param screenName
    */
   trackView(screenName) {
-    if (ga && environment.use_tracking) { // It is possible that the google script is not loaded (e.g. ad blockers
-      if (this.is_debug) { console.log(screenName); }
-      ga('set', 'page', screenName);
-      ga('send', 'pageview');
-      return true;
+    try {
+      if (typeof ga === 'function' && environment.use_tracking) { // It is possible that the google script is not loaded (e.g. ad blockers
+        if (this.is_debug) {
+          console.log(screenName);
+        }
+        ga('set', 'page', screenName);
+        ga('send', 'pageview');
+        return true;
+      }
+    } catch (e) {
+      if (this.is_debug) {
+        console.error(e);
+      }
     }
     return false;
   }
@@ -58,15 +82,23 @@ export class AnalyticsService {
    * @param value
    */
   trackEvent(category, action, label?, value?) {
-    if (ga && environment.use_tracking) { // It is possible that the google script is not loaded (e.g. ad blockers)
-      if (this.is_debug) { console.log('Category: %s, Action: %s', category, action); }
-      ga('send', 'event', {
-        eventCategory: category,
-        eventLabel: label,
-        eventAction: action,
-        eventValue: value
-      });
-      return true;
+    try {
+      if (typeof ga === 'function' && environment.use_tracking) { // It is possible that the google script is not loaded (e.g. ad blockers)
+        if (this.is_debug) {
+          console.log('Category: %s, Action: %s', category, action);
+        }
+        ga('send', 'event', {
+          eventCategory: category,
+          eventLabel: label,
+          eventAction: action,
+          eventValue: value
+        });
+        return true;
+      }
+    } catch (e) {
+      if (this.is_debug) {
+        console.error(e);
+      }
     }
     return false;
   }
