e3d22f9d6c7b0166c1416b251014c1877bf3073a
Kyle_ <kyle.e.defreitas@gmail.com>
2019-03-07 23:07:59 -0400

    updated production variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,16 +1,16 @@
 export const environment = {
   production: true,
   use_service_worker: true,
-  google_analytics_id: '',
+  google_analytics_id: 'UA-132994026-1',
   use_tracking: true,
-  is_debug: true
+  is_debug: false
 };
 
 export const firebase = {
-  apiKey: "AIzaSyDyB5983-hEsuDPEsEjliAwnWxbJmyPyXU",
-  authDomain: "wsis-admin.firebaseapp.com",
-  databaseURL: "https://wsis-admin.firebaseio.com",
-  projectId: "wsis-admin",
-  storageBucket: "wsis-admin.appspot.com",
-  messagingSenderId: "75951266374"
+  apiKey: "AIzaSyB42T8YHEhg-A8mnLx2-05wP7i4Y25ZgTI",
+  authDomain: "wsis-sdg.firebaseapp.com",
+  databaseURL: "https://wsis-sdg.firebaseio.com",
+  projectId: "wsis-sdg",
+  storageBucket: "wsis-sdg.appspot.com",
+  messagingSenderId: "3309621460"
 };
