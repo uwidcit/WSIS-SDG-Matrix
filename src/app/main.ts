57e1e0a543059acf1904d4c030d0deb7d900d340
Kyle_ <kyle.e.defreitas@gmail.com>
2019-04-03 10:17:24 -0400

    Removed spinning animation on splash screen

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,5 +1,9 @@
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './app.module';
+import {enableProdMode} from "@angular/core";
+import {environment} from "@env";
 
-// if (environment.production) { enableProdMode(); }
+if (environment.production) {
+  enableProdMode();
+}
 platformBrowserDynamic().bootstrapModule(AppModule);
